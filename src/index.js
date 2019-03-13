b58208e7def7d8df9ac63ecb7e6c42ab2e4e2cfe
ChristopherSparling <christopher.j.sparling@gmail.com>
2019-03-13 17:55:57 -0400

    Modifying file structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './style.css';
+
+// JSX
+ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragadsfsdfraph</p></div>, document.getElementById("root"))
